c997483144d346d5659d451826c3e7f0a205d796
hic-iwasaki <iwasaki@hic-soft.co.jp>
2022-11-13 14:55:24 +0900

    Javaの学習教材 - 課題の追加

--- step01/src/main/java/com/example/Chapter05.java
+++ step01/src/main/java/com/example/Chapter05.java
@@ -31,7 +31,7 @@ public final class Chapter05 {
 
     /**
      * 四則演算の書かれた文字列を数字と記号で分解しましょう。
-     * 使われる文字列数値以外に "+", "-", "×", "÷", "(", ")" があるものとします。
+     * 使われる文字列は正の整数値以外に "+", "-", "×", "÷", "(", ")" があるものとします。
      * 例）
      *  入力: "1*(40-15)"
      *  戻り: {"1", "*", "(", "40", "-", "15", ")"}
@@ -43,15 +43,101 @@ public final class Chapter05 {
     }
 
     /**
+     * step.EX: 整数の書かれた文字列を2つ受け取って足し算の答えを返しましょう。
+     * 例）
+     *  入力: "1", "1"
+     *  戻り: "2"
+     * @param num1 整数の書かれた文字列
+     * @param num2 整数の書かれた文字列
+     * @return 計算結果の文字列
+     */
+    public String addition_string_num(String num1, String num2) {
+        return null;
+    }
+
+    /**
+     * step.EX: 整数の書かれた文字列を2つ受け取って引き算の答えを返しましょう。
+     * 例）
+     *  入力: "1", "1"
+     *  戻り: "0"
+     * @param num1 整数の書かれた文字列
+     * @param num2 整数の書かれた文字列
+     * @return 計算結果の文字列
+     */
+    public String subtraction_string_num(String num1, String num2) {
+        return null;
+    }
+
+    /**
+     * step.EX: 整数の書かれた文字列を2つ受け取って掛け算の答えを返しましょう。
+     * 例）
+     *  入力: "2", "3"
+     *  戻り: "6"
+     * @param num1 整数の書かれた文字列
+     * @param num2 整数の書かれた文字列
+     * @return 計算結果の文字列
+     */
+    public String multiplication_string_num(String num1, String num2) {
+        return null;
+    }
+
+    /**
+     * step.EX: 整数の書かれた文字列を2つ受け取って割り算の答えを返しましょう。
+     * 例）
+     *  入力: "6", "2"
+     *  戻り: "3"
+     * @param num1 整数の書かれた文字列
+     * @param num2 整数の書かれた文字列
+     * @return 計算結果の文字列
+     */
+    public String division_string_num(String num1, String num2) {
+        return null;
+    }
+
+    /**
      * step.EX: 四則演算の書かれた文字列を受け取って答えを返しましょう。
-     * 使われる文字列数値以外に "+", "-", "×", "÷", "(", ")" があるものとします。
+     * 使われる文字列は正の整数値以外に "+", "-" があるものとします。
+     * 例）
+     *  入力: "8+7-10"
+     *  戻り: 5
+     * @param num_text 四則演算の指揮が書かれた文字列
+     * @return 計算結果の数値
+     */
+    public int qEX_calculator01(String formula_text) {
+        List<String> words = q90_split_words(formula_text);
+
+        return 0;
+    }
+
+    /**
+     * step.EX: qEX_calculator01 を改良して掛け算と割り算に対応させましょう。
+     * 使われる文字列は正の整数値以外に "+", "-", "×", "÷" があるものとします。
+     * また割り算の答えは必ず割り切れるものとします。
+     * 例）
+     *  入力: "7+12÷2×3"
+     *  戻り: 25
+     * @param num_text 四則演算の指揮が書かれた文字列
+     * @return 計算結果の数値
+     */
+    public int qEX_calculator02(String formula_text) {
+        List<String> words = q90_split_words(formula_text);
+
+        return 0;
+    }
+
+    /**
+     * step.EX: qEX_calculator01 を改良して括弧に対応させましょう。
+     * 使われる文字列は正の整数値以外に "+", "-", "×", "÷", "(", ")" があるものとします。
+     * また割り算の答えは必ず割り切れるものとします。
      * 例）
      *  入力: "15+(4-2)×3"
      *  戻り: 21
      * @param num_text 四則演算の指揮が書かれた文字列
-     * @return 数値型に変換した値
+     * @return 計算結果の数値
      */
-    public int qEX_calculator(String formula_text) {
+    public int qEX_calculator03(String formula_text) {
+        List<String> words = q90_split_words(formula_text);
+
         return 0;
     }
 }
--- step01/src/test/java/com/example/Chapter05Test.java
+++ step01/src/test/java/com/example/Chapter05Test.java
@@ -33,12 +33,63 @@ class Chapter05Test {
     }
 
     @Test
+    void test_addition_string_num() {
+        Chapter05 class_under_test = new Chapter05();
+        assertEquals(class_under_test.addition_string_num("2", "43"), "45");
+        assertEquals(class_under_test.addition_string_num("9", "6"), "15");
+        assertEquals(class_under_test.addition_string_num("999", "-1000"), "-1");
+    }
+
+    @Test
+    void test_subtraction_string_num() {
+        Chapter05 class_under_test = new Chapter05();
+        assertEquals(class_under_test.subtraction_string_num("5", "3"), "2");
+        assertEquals(class_under_test.subtraction_string_num("100", "-20"), "120");
+        assertEquals(class_under_test.subtraction_string_num("23", "50"), "-27");
+    }
+
+    @Test
+    void test_multiplication_string_num() {
+        Chapter05 class_under_test = new Chapter05();
+        assertEquals(class_under_test.multiplication_string_num("5", "3"), "15");
+        assertEquals(class_under_test.multiplication_string_num("100", "-20"), "-2000");
+        assertEquals(class_under_test.multiplication_string_num("23", "50"), "1150");
+    }
+
+    @Test
+    void test_division_string_num() {
+        Chapter05 class_under_test = new Chapter05();
+        assertEquals(class_under_test.division_string_num("9", "3"), "3");
+        assertEquals(class_under_test.division_string_num("100", "-20"), "-5");
+        assertEquals(class_under_test.division_string_num("1024", "32"), "32");
+    }
+
+    @Test
+    void test_EX_calculator01() {
+        Chapter05 class_under_test = new Chapter05();
+        assertEquals(class_under_test.qEX_calculator01("100-140+40"), 0);
+        assertEquals(class_under_test.qEX_calculator01("8+7-10"), 5);
+    }
+
+    @Test
+    void test_EX_calculator02() {
+        Chapter05 class_under_test = new Chapter05();
+        assertEquals(class_under_test.qEX_calculator02("100-140+40"), 0);
+        assertEquals(class_under_test.qEX_calculator02("9+9×9-10"), 80);
+        assertEquals(class_under_test.qEX_calculator02("24÷3"), 8);
+        assertEquals(class_under_test.qEX_calculator02("8+7-10"), 5);
+        assertEquals(class_under_test.qEX_calculator02("7+12÷2×3"), 25);
+    }
+
+    @Test
     void test_EX_calculator() {
         Chapter05 class_under_test = new Chapter05();
-        assertEquals(class_under_test.qEX_calculator("100-140+40"), 0);
-        assertEquals(class_under_test.qEX_calculator("9+9×9-10"), 80);
-        assertEquals(class_under_test.qEX_calculator("24÷3"), 8);
-        assertEquals(class_under_test.qEX_calculator("15+(4-2)×3"), 21);
+        assertEquals(class_under_test.qEX_calculator03("100-140+40"), 0);
+        assertEquals(class_under_test.qEX_calculator03("9+9×9-10"), 80);
+        assertEquals(class_under_test.qEX_calculator03("24÷3"), 8);
+        assertEquals(class_under_test.qEX_calculator03("15+(4-2)×3"), 21);
+        assertEquals(class_under_test.qEX_calculator03("8+7-10"), 5);
+        assertEquals(class_under_test.qEX_calculator03("7+12÷2×3"), 25);
     }
 
 }
